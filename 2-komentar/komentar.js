8105157bd788d0ae0f887bbf2d33fcdae4260753
Arief-Karditya <nolifegansaya@gmail.com>
2024-05-19 20:14:36 +0700

    Komentar, JavaScript

---
+++ 2-komentar/komentar.js
@@ -0,0 +1,12 @@
+// Ini adalah komentar.
+document.writeln("<p>Hello komentar</p>");
+
+/** Ini juga komentar. tidak akan dieksekusi oleh browser. */
+document.writeln("<p>Hello komentar dari paragraph kedua.</p>");
+
+/** 
+ * Ini juga komentar.
+ * Bisa lebih dari 1 baris.
+*/
+document.writeln("<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio velit, deserunt nulla provident molestias doloribus voluptatum, numquam voluptates qui eum perferendis ex quaerat laborum? Obcaecati eligendi corporis aperiam illum tempora</p>");
+// Komentar tidak akan dieksekusi oleh browser.
