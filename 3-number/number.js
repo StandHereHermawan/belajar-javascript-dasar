0c765d8d5106b005105a9f0320865d5751a195b7
Arief-Karditya <nolifegansaya@gmail.com>
2024-05-20 22:07:31 +0700

    Number, JavaScript

---
+++ 3-number/number.js
@@ -0,0 +1,34 @@
+/**
+ * Bilangan Bulat.
+ */
+document.writeln("Bilangan Bulat:");
+document.writeln(100);
+document.writeln("<br>");
+
+/**
+ * Bilangan Desimal.
+ */
+document.writeln("Bilangan Desimal: ");
+document.writeln(100.123);
+document.writeln("<br>");
+
+/**
+ * Notasi Bilangan Biner.
+ */
+document.writeln("Basis 10 dari Bilangan Binary 0b1010:");
+document.writeln(0b1010);
+document.writeln("<br>");
+
+/**
+ * Notasi Bilangan HeksaDesimal.
+ */
+document.writeln("Basis 10 dari Bilangan Heksadesimal 0xFF:");
+document.writeln(0xFF);
+document.writeln("<br>");
+
+/**
+ * Notasi Bilangan Oktal.
+ */
+document.writeln("Basis 10 dari Bilangan Oktal 0o67:");
+document.writeln(0x67);
+document.writeln("<br>");
