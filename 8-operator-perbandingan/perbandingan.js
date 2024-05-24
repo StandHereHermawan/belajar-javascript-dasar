fa66caf79c9b680e06574248353b3ec0174bca5d
Arief-Karditya <nolifegansaya@gmail.com>
2024-05-24 10:03:19 +0700

    Operator Perbandingan, JavaScript

---
+++ 8-operator-perbandingan/perbandingan.js
@@ -0,0 +1,11 @@
+let result = 5 == "5";
+document.writeln('<p> 5 == "5" = ' + result + "</p>");
+
+result = 5 === "5";
+document.writeln('<p> 5 === "5" = ' + result + "</p>");
+
+result = 5 > 10;
+document.writeln("<p> 5 > 10 = " + result + "</p>");
+
+result = 5 < 10;
+document.writeln("<p> 5 < 10 = " + result + "</p>");
