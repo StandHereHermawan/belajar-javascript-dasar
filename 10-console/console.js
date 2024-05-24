a5ff835bc27767e0f695467219ab09c2c1d891f8
Arief-Karditya <nolifegansaya@gmail.com>
2024-05-24 10:03:53 +0700

    Console, JavaScript

---
+++ 10-console/console.js
@@ -0,0 +1,19 @@
+/**
+ * Memberitahu Informasi. untuk keperluan sekecil mungkin.
+ */
+console.debug("Hello, debug Console Browser");
+
+/**
+ * Memberitahu Informasi.
+ */
+console.info("Hello, info Console Browser");
+
+/**
+ * Memberitahu Informasi peringatan.
+ */
+console.warn("Hello, warn Console Browser");
+
+/**
+ * Memberitahu Informasi error.
+ */
+console.error("Hello, error Console Browser");
