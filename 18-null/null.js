6fef3627a80dfef1ece4f7f37dde90bb668b6f38
Arief-Karditya <nolifegansaya@gmail.com>
2024-05-26 21:09:17 +0700

    Null, JavaScript

---
+++ 18-null/null.js
@@ -0,0 +1,32 @@
+let firstName; // undefined.
+
+if (firstName === undefined) {
+    alert("UNDEFINED");
+} else if (firstName === null){
+    alert("NULL");
+} else {
+    alert(firstName);
+}
+
+/**
+ * Tipe data Null. merepresentasikan data kosong.
+*/
+firstName = null; // null.
+
+if (firstName === undefined) {
+    alert("UNDEFINED");
+} else if (firstName === null){
+    alert("NULL");
+} else {
+    alert(firstName);
+}
+
+firstName = "Arief";
+
+if (firstName === undefined) {
+    alert("UNDEFINED");
+} else if (firstName === null){
+    alert("NULL");
+} else {
+    alert(firstName);
+}
