ca0561931d04dba74b7306bad122000b9552643b
Arief-Karditya <nolifegansaya@gmail.com>
2024-05-27 22:46:41 +0700

    Operator logika pada non Boolean, JavaScript

--- 26-operator-logika-pada-non-boolean/logika-non-boolean.js
+++ 26-operator-logika-pada-non-boolean/logika-non-boolean.js
@@ -1,13 +1,13 @@
 /**
  * Operator OR
  */
-console.info("Hello" || "");
-console.info("" || []);
-console.info("0, String" || "NOL"); // keduanya meski bernilai true alias truthy, tetapi nilai paling kiri diambil.
-console.info(0 || "NOL, String");
-console.info(null || "NULL, String.");
-console.info(undefined || "UNDEFINED, String");
-console.info("" || false); // keduanya meski bernilai false alias falsey, nilai paling kanan yang diambil.
+console.info("Hello" || ""); // "Hello" dipilih.
+console.info("" || []); // [] dipilih.
+console.info("0, String" || "NOL"); // "0, string" dipilih. kedua parameternya meski bernilai true alias truthy, tetapi nilai paling kiri diambil.
+console.info(0 || "NOL, String"); // "NOL, String" dipilih.
+console.info(null || "NULL, String."); // "NULL, String." dipilih.
+console.info(undefined || "UNDEFINED, String"); // "UNDEFINED, String" dipilih.
+console.info("" || false); // false dipilih. keduanya meski bernilai false alias falsey, nilai paling kanan yang diambil.
 
 // use case operator logika non boolean dengan falsey atau truthy.
 let person = {
@@ -29,10 +29,10 @@ console.info(name);
 /**
  * Operator AND
  */
-console.info("HELLO" && "");
-console.info("" && []);
-console.info("0" && "NOL"); // Jika kedua nilai adalah truthy, Nilai paling kanan yang diambil.
-console.info(0 && "NOL");
-console.info(null && "NULL");
-console.info(undefined && "UNDEFINED");
-console.info("undefined" && "null");
+console.info("HELLO" && ''); // '' dipilih.
+console.info("" && []); // "" dipilih.
+console.info("0" && "NOL"); // "NOL" dipilih. Jika kedua nilai adalah truthy, Nilai paling kanan yang diambil.
+console.info(0 && "NOL"); // 0 dipilih.
+console.info(null && "NULL"); // null dipilih.
+console.info(undefined && "UNDEFINED"); // undefined dipilih.
+console.info("undefined" && "null"); // "null" dipilih.
