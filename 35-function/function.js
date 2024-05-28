cf8fa448c18246dd3328fe77645eab9c5239a907
Arief-Karditya <nolifegansaya@gmail.com>
2024-05-28 23:04:31 +0700

    Function, JavaScript

---
+++ 35-function/function.js
@@ -0,0 +1,13 @@
+/**
+ * Membuat function.
+ */
+function sayHelloWorld() {
+  document.writeln("<p>Hello World!</p>");
+}
+
+/**
+ * Memanggil function.
+ */
+sayHelloWorld();
+sayHelloWorld();
+sayHelloWorld();
