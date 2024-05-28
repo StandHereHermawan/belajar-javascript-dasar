284cf5f7800b1f5cb807e82dcda9fa31831d84ed
Arief-Karditya <nolifegansaya@gmail.com>
2024-05-28 23:04:44 +0700

    Function Parameter, JavaScript

---
+++ 36-function-parameter/function-parameter.js
@@ -0,0 +1,12 @@
+/**
+ * Membuat function dengan parameter.
+ */
+function sayHello(firstName, lastName) {
+  document.writeln(`<p>Hello ${firstName} ${lastName}.</p>`);
+}
+
+/**
+ * Memanggil function dengan parameter.s
+ */
+sayHello("Arief", "Hermawan");
+sayHello("Hilmi", "Muharrom");
